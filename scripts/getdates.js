b4d798db8d14065e36a54553a855cd33f2c480c4
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-03-08 16:08:41 -0700

    Fixed issues

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currently").innerHTML = year;
+
+const dd = new Date();
+document.getElementById("lastModified").innerHTML = dd;
