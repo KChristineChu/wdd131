1abbac7a035bf2598d4fbf46239b012a58e50403
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-03-15 14:42:15 -0600

    Worked on JS

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,10 @@
+const hambutton = document.querySelector("#menu");
+const nav = document.querySelector(".header-nav");
 
-
-
+hambutton.addEventListener("click", () => {
+    nav.classList.toggle("show");
+    hambutton.classList.toggle("show");
+});
 
 let myDate = new Date();
 let myYear = myDate.getFullYear();
