aebac82ef02d7d126efb7dfd7b9d1b17a95da3e4
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-03-22 15:53:57 -0600

    Worked on media query and JavaScript

--- scripts/place.js
+++ scripts/place.js
@@ -1,1 +1,26 @@
 
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("currently").textContent = myYear;
+
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+
+//Calculate wind chill with fahrenheit and wind speed
+const temperature = 57;
+const windSpeed = 5;
+
+function calculateWindChill(temp, wind) {
+    return (
+        35.74 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16).toFixed(1)
+    );
+}
+function displayWindChill() {
+    let windChillElement = document.getElementById("windchill");
+    let windchill = "N/A";  //default value
+
+    if (temperature <= 50 && windSpeed > 3) {
+        windchill = calculateWindChill(temperature, windSpeed) + " °F";
+    }
+
+    windChillElement.textContent = windChill;
+}
