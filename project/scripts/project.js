ecd7dc637d793bd83a3e65ebe40bbfa6ddb4ac7c
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-04-08 22:34:31 -0600

    Completed index.html

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -5,3 +5,9 @@ hambutton.addEventListener("click", () => {
     mainnav.classList.toggle("show");
     hambutton.classList.toggle("show");
 });
+
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("currently").textContent = myYear;
+
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
