6d4105719c15a064ef689ffea5c8b41d34eabb83
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-04-10 02:15:11 -0600

    fixed hero image size

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,3 +1,4 @@
+//hamburger menu
 const mainnav = document.querySelector(".navigation")
 const hambutton = document.querySelector("#menu");
 
@@ -6,8 +7,111 @@ hambutton.addEventListener("click", () => {
     hambutton.classList.toggle("show");
 });
 
+//Set modified date and time
 let myDate = new Date();
 let myYear = myDate.getFullYear();
 document.getElementById("currently").textContent = myYear;
 
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+
+//Array of tours
+
+const tours = [
+    {
+        location: "Great Britain",
+        numberOfDays: 8,
+        image: "images/london.webp"
+    },
+
+    {
+        location: "France",
+        numberOfDays: 9,
+        image: "images/France.webp"
+    },
+
+    {
+        location: "Greece",
+        numberOfDays: 9,
+        image: "images/greece.webp"
+    },
+
+    {
+        location: "Germany",
+        numberOfDays: 8,
+        image: "images/germany.webp"
+    },
+
+    {
+        location: "Ireland",
+        numberOfDays: 5,
+        image: "images/ireland.webp"
+    },
+
+    {
+        location: "Italy",
+        numberOfDays: 8,
+        image: "images/italy.webp"
+    },
+
+    {
+        location: "Belgium",
+        numberOfDays: 4,
+        image: "images/belgium.webp"
+    },
+
+    {
+        location: "Switzerland",
+        numberOfDays: 5,
+        image: "images/switzerland.webp"
+    },
+
+    {
+        location: "Spain",
+        numberOfDays: 6,
+        image: "images/spain.webp"
+    },
+];
+
+function createTourCard(tours) {
+    document.querySelector(".container2").innerHTML = "";
+
+    tours.forEach(tour => {
+        let card = document.createElement("section");
+        let location = document.createElement("p");
+        let numberOfDays = document.createElement("p");
+        let image = document.createElement("img");
+
+        location.innerHTML = `<span class="label">Location:</span> ${tour.location}`;
+        numberOfDays.innerHTML = `<span class="label">Number of Days</span>${tour.numberOfDays}`;
+        image.classList.add("tour-img");
+        image.setAttribute("src", tour.image);
+        image.setAttribute("alt", `${tour.location}`);
+        image.setAttribute("loading", "lazy");
+
+        card.appendChild(location);
+        card.appendChild(numberOfDays);
+        card.appendChild(image);
+
+        document.querySelector(".container2").appendChild(card);
+    });
+}
+
+
+function allTours() {
+    createTourCard(tours);
+}
+
+function shortTours() {
+    let shortTours = tours.filter(tour => tour.numberOfDays < 7);
+    createTourCard(shortTours);
+}
+
+function longTours() {
+    let longTours = tours.filter(tour => tour.numberOfDays >= 7);
+    createTourCard(longTours);
+}
+
+document.querySelector("#allTours").addEventListener("click", allTours);
+document.querySelector("#shortTours").addEventListener("click", shortTours);
+document.querySelector("#longTours").addEventListener("click", longTours);
+allTours();
